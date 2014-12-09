9bf117135dfdce77bb4e76b0aa492f5b19e98503
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-08 20:35:34 -0500

    mock out api factory

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -2,12 +2,44 @@
 
 /**
  * @ngdoc service
- * @name letsgoApp.api
+ * @name letsgoApp.api.factory
  * @description
- * # api
- * Service in the letsgoApp.
+ *
+ * This houses all of the calls to the navigator API
  */
-angular.module('letsgoApp')
-  .service('api', function api() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+angular.module('letsgoApp.api.factory')
+  .factory('api', [
+    '$http',
+
+    function($http) {
+      'use strict';
+
+    // Public API here
+    return {
+      /**
+       * Get list of all available tables
+       * @returns {HttpPromise}
+       */
+      tables: function () {
+        var url = config.apiRoot + "/geo";
+        return $http.get(url);
+      },
+      /**
+       * Get results for a single table
+       * @param table: Table name
+       * @returns {HttpPromise}
+       */
+      table: function (table) {
+        var url = config.apiRoot + "/geo/" + table;
+        return $http.get(url);
+      },
+      /**
+       * Solve a route
+       * @returns {HttpPromise}
+       */
+      route: function () {
+        var url = config.apiRoot + "/route";
+        return $http.get(url);
+      }
+    };
+  }]);
