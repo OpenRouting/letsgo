3f3cb28d863b40c9a5edc4c6c211311f64244f72
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-08 20:44:43 -0500

    create configuration

---
+++ app/scripts/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by christopherfricke on 12/8/14.
+ */
+var config = new function() {
+  this.apiRoot = "http://routingservice-bmoregeoweb.rhcloud.com/api/v1/";
+  this.map = {
+    center: [],
+    zoom: 20,
+    layers:{
+      name: {info:"info"}
+    }
+  }
+};
