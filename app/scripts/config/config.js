cf0a0aa7efb6b9860c405b48cd1ea1e2478f672f
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-08 20:52:42 -0500

    clean up configuration

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,13 +1,14 @@
 /**
  * Created by christopherfricke on 12/8/14.
  */
+"use strict";
 var config = new function() {
-  this.apiRoot = "http://routingservice-bmoregeoweb.rhcloud.com/api/v1/";
+  this.apiRoot = 'http://routingservice-bmoregeoweb.rhcloud.com/api/v1/';
   this.map = {
     center: [],
     zoom: 20,
     layers:{
-      name: {info:"info"}
+      name: {info:'info'}
     }
   }
 };
