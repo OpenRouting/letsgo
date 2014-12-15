bf50ddc366a8659845eb496e2eefc46791e7fe8d
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-14 20:30:37 -0500

    add in sidepanel and maps

---
+++ app/scripts/controllers/sidepanel.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name letsgo.controller:SidePanelCtrl
+ * @description
+ * # MapCtrl
+ * Controller of the letsgoApp
+ */
+angular.module('letsgo')
+  .controller('SidePanelCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
