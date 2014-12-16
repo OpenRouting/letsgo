352678dbb39a79396cad7b6148436bfeefd3d50a
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-15 23:30:37 -0500

    style map

--- app/scripts/directives/leaflet/leaflet.directive.js
+++ app/scripts/directives/leaflet/leaflet.directive.js
@@ -5,6 +5,16 @@ angular.module('leaflet', [])
 .directive('leaflet', [function() {
 
   function link(scope, element, attrs) {
+    var map = L.map('map', {
+      center: scope.center,
+      zoom: scope.zoom
+    });
+
+
+    L.tileLayer(scope.basemaps[0].url, {
+      attribution: scope.basemaps[0].credits,
+      id: scope.basemaps[0].mapboxId
+    }).addTo(map);
 
   }
 
@@ -14,7 +24,8 @@ angular.module('leaflet', [])
     scope: {
       center: '=',
       zoom: '=',
-      layers: '='
+      layers: '=',
+      basemaps: '='
     },
     templateUrl: 'scripts/directives/leaflet/leaflet.tpl.html',
     link: link
--- app/scripts/map/map.ctrl.js
+++ app/scripts/map/map.ctrl.js
@@ -7,11 +7,23 @@
  * # MapCtrl
  * Controller of the letsgoApp
  */
-angular.module('letsgo.map', [])
+angular.module('letsgo.map', [
+  'leaflet'
+])
   .controller('MapCtrl', function ($scope) {
     $scope.pm = {
-      center: [-77,10],
-      zoom:22,
-      layers: []
+      center: [39.283333,-76.616667],
+      zoom:12,
+      layers: [],
+      basemaps:[
+        {
+          url: 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
+          credits: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+          mapboxId: 'examples.map-i875mjb7'
+
+        }
+      ]
     };
   });
