5c162fa13532e0c2d2c8d4e3e8ee2e69b261b0e1
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-15 20:35:54 -0500

    reformat folder structure to follow more module style

---
+++ app/scripts/directions/direction.ctrl.js
@@ -0,0 +1,15 @@
+/**
+ * @ngdoc function
+ * @name letsgo.controller:MapCtrl
+ * @description
+ * # MapCtrl
+ * Controller of the letsgoApp
+ */
+angular.module('letsgo.directions')
+  .controller('DirectionCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
