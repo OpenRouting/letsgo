320e3339620e21b20b5add38eee27064e198b834
christopherfricke <christopher.fricke@bmoregeo.com>
2014-12-20 17:08:00 -0500

    switch attributes to follow geojson format

--- app/scripts/map/map.ctrl.js
+++ app/scripts/map/map.ctrl.js
@@ -8,23 +8,84 @@
  * Controller of the letsgoApp
  */
 angular.module('letsgo.map', [
-  'leaflet'
-])
-  .controller('MapCtrl', function ($scope) {
-    $scope.pm = {
-      center: [38.929,-77.051],
-      zoom:16,
-      layers: [],
-      basemaps:[
-        {
-          name: 'MapBox Demo',
-          url: 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
-          credits: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-          mapboxId: 'examples.map-i875mjb7'
+  'leaflet-directive',
+  'letsgo.pointofinterest'
 
+])
+  .controller('MapCtrl', ['$scope', 'poiService', function ($scope, poiService) {
+    $scope.pm = {
+      center: {
+        lat: 38.929,
+        lng: -77.051,
+        zoom: 16
+      },
+      defaults: {
+        tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+        maxZoom: 14,
+        path: {
+          weight: 10,
+          color: '#800000',
+          opacity: 1
         }
-      ]
+      },
+      layers: [],
+
+      geojson: {
+        data: undefined,
+        style: {
+          "color": "#ff7800",
+          "weight": 5,
+          "opacity": 0.65
+        }
+      },
+
+      basemaps: {
+        "baselayers": {
+          "osm": {
+            "name": "OpenStreetMap",
+            "url": "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            "type": "xyz",
+            "layerParams": {},
+            "layerOptions": {}
+          },
+          "mapbox_terrain": {
+            "name": "Mapbox Terrain",
+            "url": "http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}",
+            "type": "xyz",
+            "layerOptions": {
+              "apikey": "pk.eyJ1IjoidG9tYmF0b3NzYWxzIiwiYSI6Imo3MWxyTHMifQ.TjXg_IV7ZYMHX6tqjMikPg",
+              "mapid": "tombatossals.jbn2nnon"
+            },
+            "layerParams": {}
+          }
+        }
+      }
     };
-  });
+
+    $scope.poiService = poiService;
+
+    $scope.$watch(
+      function(){return $scope.poiService.pm.poi},
+      function(){
+        angular.extend($scope, {
+          geojson: {
+            data: {
+              "type": "FeatureCollection",
+              "features": $scope.poiService.pm.poi
+            },
+            style: {
+              "color": "#ff7800",
+              "weight": 5,
+              "opacity": 0.65
+            },
+            resetStyleOnMouseout: true
+          }
+          }
+        );
+
+
+      },
+      true
+
+    );
+  }]);
